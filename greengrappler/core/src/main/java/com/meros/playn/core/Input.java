8136960b950e12a4a316fbc2148d5e7d6ffec0f1
Alexander Schrab <meros@Alexanders-MacBook-Air.local>
2012-06-30 08:04:49 +0200

    Forgot the new INput class

---
+++ greengrappler/core/src/main/java/com/meros/playn/core/Input.java
@@ -0,0 +1,21 @@
+package com.meros.playn.core;
+
+import com.meros.playn.core.Constants.Buttons;
+
+public class Input {
+	
+	public static boolean isHeld(Buttons aButton) {
+		// TODO Auto-generated method stub
+		return aButton == Buttons.Right;
+	}
+
+	public static boolean isPressed(Buttons aButton) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean isReleased(Buttons aButton) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
