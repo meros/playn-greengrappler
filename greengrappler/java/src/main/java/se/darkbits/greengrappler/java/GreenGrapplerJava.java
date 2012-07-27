22d6f097a0a5f6f621c552c28a6c34c6e49c7125
Alexander Schrab <meros@Alexanders-MacBook-Air.local>
2012-07-27 08:28:28 +0200

    forgot java files

---
+++ greengrappler/java/src/main/java/se/darkbits/greengrappler/java/GreenGrapplerJava.java
@@ -0,0 +1,45 @@
+package se.darkbits.greengrappler.java;
+
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+
+import playn.core.PlayN;
+import playn.java.JavaPlatform;
+import se.darkbits.greengrappler.GreenGrappler;
+import se.darkbits.greengrappler.media.Music;
+import se.darkbits.greengrappler.media.Music.AbstractSong;
+import se.darkbits.greengrappler.media.Music.SongFactory;
+
+
+public class GreenGrapplerJava {
+
+	public static void main(String[] args) {
+		JavaPlatform platform = JavaPlatform.register();
+		platform.assets().setPathPrefix("se/darkbits/greengrappler/resources");
+		
+		Music.setSongFactory(new SongFactory(){
+
+			@Override
+			public AbstractSong getSong(String resource) {
+				try {
+					return new Song(getClass().getClassLoader().getResourceAsStream("com/meros/playn/resources/" + resource));
+				} catch (LineUnavailableException e) {
+					return null;
+				} catch (IOException e) {
+					return null;
+				}
+			}
+		});
+		
+		PlayN.run(new se.darkbits.greengrappler.GreenGrappler(false,
+				new GreenGrappler.ExitCallback() {
+
+					@Override
+					public void exit() {
+						System.exit(0);
+
+					}
+				}));
+	}
+}
