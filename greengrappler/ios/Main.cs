221e024d610222d93e8c58d07d06d32ea0972b08
Alexander Schrab <meros@Alexanders-MacBook-Air.local>
2012-08-10 08:19:23 +0200

    Fixed iphone build sort of... Using a non-official branch of playn that fixes the graphics. Also fixed some old references to com.meros....

--- greengrappler/ios/Main.cs
+++ greengrappler/ios/Main.cs
@@ -4,7 +4,7 @@ using MonoTouch.UIKit;
 
 using playn.ios;
 using playn.core;
-using com.meros.playn.core;
+using se.darkbits.greengrappler;
 
 namespace com.meros.playn
 {
@@ -16,18 +16,10 @@ namespace com.meros.playn
       pf.assets().setPathPrefix("assets");
       PlayN.run(
 		new GreenGrappler(
-				true, 
-				new blaj()));
+				true));
       return true;
     }
   }
-
-	public class blaj : GreenGrappler.ExitCallback
-	{
-		public void exit ()
-		{
-		}
-	}
 
   public class Application {
     static void Main (string[] args) {
